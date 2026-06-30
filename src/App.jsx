a35863736b9fa7f0180cf79b361d02bc185ef503
Sandeep <sandeepgadari2001@gmail.com>
2026-07-01 01:30:44 +0530

    Added scrolling features in the title as well as paragraph section

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
    setdetails(copyDetails);
   }
   return (
-    <div className="flex  max-h-screen px-6 items-center bg-[url('https://images.unsplash.com/photo-1584099799061-455b6b0b7f00?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+    <div className="flex  max-h-screenzs px-6 items-center bg-[url('https://images.unsplash.com/photo-1584099799061-455b6b0b7f00?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
 
       <div className='  flex p-6 lg:w-1/2 items-center '>
         <form className=' px-10 py-4 flex flex-col gap-4 ' onSubmit={(e) => {
@@ -45,17 +45,17 @@ const App = () => {
           details.map(function (elem, idx) {
             return (
               // console.log(elem.title)
-              <div key={idx} className="w-48 h-52 bg-cover rounded-2xl flex flex-col flex-wrap justify-between items-center bg-[url('https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')]">
+              <div key={idx} className="w-50 h-70 bg-cover rounded-2xl flex flex-col flex-wrap justify-between items-center bg-[url('https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')]">
                 <div className=' w-full  py-5'>
-                  <h5 className=' pl-5 font-bold text-blue-400 text-xl'>{elem.title}</h5>
-                  <p className='pl-5 text-blue-400'><b>{elem.description}</b></p>
+                  <h5 id='titleHeader' className='w-50 pl-5 pt-2 font-bold text-blue-400 text-xl wrap-break-word h-[10vh] overflow-scroll'>{elem.title}</h5>
+                  <p id='paraHeader' className='pl-5 text-blue-400 wrap-break-word w-[27vh] h-[18vh] overflow-scroll'><b>{elem.description}</b></p>
                 </div>
                 <button onClick={()=>{
                   deleteAction(idx);
                 }} className=' cursor-pointer active:scale-105 mb-2 bg-red-500 w-20 rounded-2xl'>Delete</button>
               </div>
             )
-          })
+          }) 
         }
 
 
